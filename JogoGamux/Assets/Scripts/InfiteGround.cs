783bd272dd2fa122aa3c33da914cb2001d675a59
Shockwaiver <38588489+Shockwaiver@users.noreply.github.com>
2018-04-22 18:02:57 -0300

    Jogo

---
+++ JogoGamux/Assets/Scripts/InfiteGround.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiteGround : MonoBehaviour {
+
+	public GameObject Ground;
+	public Transform GenerationPoint;
+
+	private float GroundWidht;
+
+	// Use this for initialization
+	void Start () {
+
+		GroundWidht = Ground.GetComponent<BoxCollider2D> ().size.x;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.position.x < GenerationPoint.position.x) {
+		
+			transform.position = new Vector3 (transform.position.x + GroundWidht, transform.position.y, transform.position.z);
+
+			Instantiate (Ground, transform.position, transform.rotation);
+		
+		}
+
+	}
+}
---
+++ JogoGamux/Assets/Scripts/Pedra.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pedra : MonoBehaviour {
+
+	public Rigidbody2D pedra;
+	public Rigidbody2D estilingue;
+
+
+	public float ReleaseTime = .15f;
+	public float MaxDragDistance;
+
+	private bool IsPressed = false;
+
+	void Update()
+	{
+		if (IsPressed) {
+			
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+			if (Vector3.Distance (mousePos, estilingue.position) > MaxDragDistance) {
+				pedra.position = estilingue.position + (mousePos - estilingue.position).normalized * MaxDragDistance;
+			} else {
+				pedra.position = mousePos;
+			}
+		}
+	}
+
+	void OnMouseDown ()
+	{
+
+		IsPressed = true;
+	
+	}
+
+	void OnMouseUp()
+	{
+		pedra.isKinematic = false;
+		IsPressed = false;
+		pedra.velocity = new Vector2 (pedra.velocity.x * 1000, pedra.velocity.y * 1000);
+		StartCoroutine (Release ());
+	}
+
+	IEnumerator Release()
+	{
+		yield return new WaitForSeconds (ReleaseTime);
+
+		GetComponent<SpringJoint2D> ().enabled = false;
+	}
+
+
+}
