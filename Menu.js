cf9e4dab404169c332a0cd3803a6f821419a385a
LucasAzzzz <37451939+LucasAzzzz@users.noreply.github.com>
2018-04-24 19:15:22 -0300

    Create Menu.js

---
+++ Menu.js
@@ -0,0 +1,21 @@
+source code
+		
+var Skin : GUISkin;
+		
+		
+		function Update (){
+		}
+		 function OnGUI (){
+			
+			GUI.skin = Skin;
+
+			if(GUI.Button(Rect(Screen.width/2, Screen.height/2,90,90),"JOGAR"))
+			}
+			Application.LoadLevel("Jogo1");
+			}
+
+			if (GUI,Button(Rect(Screen.width/2, Screen.height/2 +90,90,90),"SAIR"))
+			{
+				Application.Quit();
+			}
+				VAI SE FUDE JAPONES UNITY MERDA OBG.
